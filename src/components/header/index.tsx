5a41b35d56dcd203dc96f125db906e947e42118d
Matheus <matheusfariasdasneves@gmail.com>
2025-04-27 12:27:39 -0300

    fix: ensure the position fixed workin correctly

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,7 @@ import { Button } from '../ui/button'
 
 export function Header() {
   return (
-    <header className="min-14 sticky top-0 z-10 flex h-14 w-full items-center justify-between border border-b-gray-200 bg-white/80 px-2 backdrop-blur-xs">
+    <header className="fixed top-0 left-0 z-10 flex h-14 min-h-14 w-full items-center justify-between border border-b-gray-200 bg-white/90 px-2 backdrop-blur-xs">
       <Button variant="ghost" className="hover:cursor-pointer">
         <Menu />
       </Button>
