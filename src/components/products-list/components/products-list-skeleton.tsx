90c58bac325796c0efde9f9a31e61461e4804ffc
Matheus <matheusfariasdasneves@gmail.com>
2025-04-28 20:27:03 -0300

    feat: add ProductsListSkeleton component

---
+++ src/components/products-list/components/products-list-skeleton.tsx
@@ -0,0 +1,11 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
+export function ProductsListSkeleton() {
+  return (
+    <div className="mx-auto flex flex-col gap-1.5">
+      <Skeleton className="h-48 w-44 rounded-[10px] sm:w-48 md:w-56" />
+      <Skeleton className="h-4 w-36 rounded-[10px] sm:w-40" />
+      <Skeleton className="h-4 w-28 rounded-[10px] sm:w-32" />
+    </div>
+  )
+}
