91353c080af6e60e8bd03bdc4e08af660efd1cb4
Matheus <matheusfariasdasneves@gmail.com>
2025-04-27 21:21:50 -0300

    feat: add image domains config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  images: {
+    domains: ['i5wancfdm9.ufs.sh'],
+  },
 }
 
 export default nextConfig
