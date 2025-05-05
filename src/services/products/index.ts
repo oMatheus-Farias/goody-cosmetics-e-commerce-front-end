f8325f5bf4246703ad2662792a6b8eda22fa6f2b
Matheus <matheusfariasdasneves@gmail.com>
2025-05-05 20:52:23 -0300

    feat: add getProductById method in ProductsService

--- src/services/products/index.ts
+++ src/services/products/index.ts
@@ -2,15 +2,21 @@ import { httpClient } from '@/configs/http-client'
 
 import type {
   IGetAllProductsByCategoryIdProps,
-  IGetAllProductsByCategoryIdReturn,
+  IGetProductReturn,
 } from './interfaces'
 
 export class ProductsService {
+  static async getProductById(productId: string) {
+    const { data } = await httpClient.get<IGetProductReturn>(
+      `/api/products/${productId}`,
+    )
+    return data
+  }
   static async getAllProductsByCategoryId({
     categoryId,
     ordenation,
   }: IGetAllProductsByCategoryIdProps) {
-    const { data } = await httpClient.get<IGetAllProductsByCategoryIdReturn[]>(
+    const { data } = await httpClient.get<IGetProductReturn[]>(
       '/api/products/category',
       {
         params: {
