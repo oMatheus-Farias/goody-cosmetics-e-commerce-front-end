e6e683c4aadc905ecba93d47507903db51194ed4
Matheus <matheusfariasdasneves@gmail.com>
2025-05-05 20:51:27 -0300

    feat: add container in HeroBannersCarousel component

--- src/components/hero-banners-carousel/index.tsx
+++ src/components/hero-banners-carousel/index.tsx
@@ -23,36 +23,38 @@ export function HeroBannersCarousel() {
   )
 
   return (
-    <Carousel
-      plugins={[plugin.current]}
-      className="mt-4 w-full"
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
-    >
-      <CarouselContent className="relative ml-0 w-full">
-        {HERO_BANNER_DATA.map((item) => (
-          <CarouselItem key={item.id} className="pr-0 pl-0">
-            <div className="relative h-52 w-full overflow-hidden rounded-2xl sm:h-72 lg:h-[400px]">
-              <Image
-                src={item.image.srcMobile}
-                alt={item.image.alt}
-                fill
-                sizes="full"
-                className="object-cover md:hidden"
-              />
-              <Image
-                src={item.image.srcDesktop}
-                alt={item.image.alt}
-                fill
-                sizes="full"
-                className="hidden object-cover md:block"
-              />
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className="absolute left-1" />
-      <CarouselNext className="absolute right-1" />
-    </Carousel>
+    <div className="w-full">
+      <Carousel
+        plugins={[plugin.current]}
+        className="mt-4 w-full"
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+      >
+        <CarouselContent className="relative ml-0 w-full gap-1">
+          {HERO_BANNER_DATA.map((item) => (
+            <CarouselItem key={item.id} className="pr-0 pl-0">
+              <div className="relative h-52 w-full overflow-hidden rounded-2xl sm:h-72 lg:h-[400px]">
+                <Image
+                  src={item.image.srcMobile}
+                  alt={item.image.alt}
+                  fill
+                  sizes="full"
+                  className="object-cover md:hidden"
+                />
+                <Image
+                  src={item.image.srcDesktop}
+                  alt={item.image.alt}
+                  fill
+                  sizes="full"
+                  className="hidden object-cover md:block"
+                />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="absolute left-1" />
+        <CarouselNext className="absolute right-1" />
+      </Carousel>
+    </div>
   )
 }
