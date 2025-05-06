e1593e874a92396e98f1142478b50461e8b014aa
Matheus <matheusfariasdasneves@gmail.com>
2025-05-06 11:35:42 -0300

    feat: add ProductsDetailCarousel component

---
+++ src/app/product-detail/_components/products-detail-carousel/index.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+import type { IGetProductReturn } from '@/services/products/interfaces'
+
+type TProps = {
+  product: IGetProductReturn | undefined
+}
+
+export function ProductsDetailCarousel({ product }: TProps) {
+  return (
+    <Carousel className="w-full lg:w-2/3">
+      <CarouselContent className="relative ml-0 w-full gap-1">
+        {product &&
+          product.productImage.map((item) => (
+            <CarouselItem key={item.id} className="pr-0 pl-0">
+              <div className="relative h-80 w-full overflow-hidden rounded-[10px] md:h-[500px] lg:h-[520px]">
+                <Image
+                  src={item.url}
+                  alt={product.name}
+                  fill
+                  sizes="full"
+                  className="object-cover"
+                />
+              </div>
+            </CarouselItem>
+          ))}
+      </CarouselContent>
+      <CarouselPrevious className="absolute left-1" />
+      <CarouselNext className="absolute right-1" />
+    </Carousel>
+  )
+}
