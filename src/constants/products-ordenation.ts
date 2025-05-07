d9c6ba0516e946bee0ed242a37615790bed41e91
Matheus <matheusfariasdasneves@gmail.com>
2025-05-07 19:10:06 -0300

    refactor: add uppercase in label of products ordenation constant

--- src/constants/products-ordenation.ts
+++ src/constants/products-ordenation.ts
@@ -1,6 +1,6 @@
 export const PRODUCTS_ORDENATION = [
   { id: 1, label: 'A-Z', value: 'A-Z' },
   { id: 2, label: 'Z-A', value: 'Z-A' },
-  { id: 4, label: 'Menor Preço', value: 'LOWER_PRICE' },
-  { id: 3, label: 'Maior Preço', value: 'HIGHER_PRICE' },
+  { id: 4, label: 'MENOR PREÇO', value: 'LOWER_PRICE' },
+  { id: 3, label: 'MAIOR PREÇO', value: 'HIGHER_PRICE' },
 ]
