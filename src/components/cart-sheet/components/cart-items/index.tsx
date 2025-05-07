9bd8914c881262181f439dbed09de765b7e909bf
Matheus <matheusfariasdasneves@gmail.com>
2025-05-07 11:29:48 -0300

    feat: add CartItems component and cart content with initial config

---
+++ src/components/cart-sheet/components/cart-items/index.tsx
@@ -0,0 +1,73 @@
+import { ChevronLeft, ChevronRight, Trash2 } from 'lucide-react'
+import Image from 'next/image'
+
+import { Button } from '@/components/ui/button'
+import type { ICartProduct } from '@/contexts/cart-context/interfaces/cart-product-interface'
+import { formatCurrency } from '@/functions/format-currency'
+
+type TProps = {
+  product: ICartProduct
+}
+
+export function CartItems({ product }: TProps) {
+  return (
+    <div className="flex w-full gap-2">
+      <Image
+        src={product.productImage[0].url}
+        alt={product.name}
+        width={100}
+        height={100}
+        sizes="all"
+        className="h-[100px] w-[100px] rounded-[10px] object-cover"
+      />
+
+      <div className="flex w-full items-center justify-between">
+        <div className="flex h-full flex-col">
+          <div className="flex-1">
+            <span className="text-sm font-light text-gray-600 uppercase">
+              {product.name}
+            </span>
+            <div className="flex items-end gap-0.5">
+              <span className="text-sm font-light text-gray-600 uppercase">
+                {formatCurrency(product.currentPrice)}
+              </span>
+              <span className="text-xs font-light text-gray-400 line-through">
+                {formatCurrency(product.oldPrice)}
+              </span>
+            </div>
+          </div>
+          <div className="flex items-center gap-4">
+            <Button
+              type="button"
+              disabled={product.quantity === 1}
+              aria-label="Diminuir quantidade"
+              className="bg-goodycosmetics-primary-300 hover:bg-goodycosmetics-primary-400 flex h-9 w-9 items-center justify-center rounded text-black transition-all duration-150 ease-linear hover:cursor-pointer disabled:bg-gray-200"
+            >
+              <ChevronLeft className="h-5 w-5" />
+            </Button>
+            <span className="text-normal font-light text-black">
+              {product.quantity}
+            </span>
+            <Button
+              type="button"
+              className="bg-goodycosmetics-primary-300 hover:bg-goodycosmetics-primary-400 flex h-9 w-9 items-center justify-center rounded text-black transition-all duration-150 ease-linear hover:cursor-pointer"
+              aria-label="Aumentar quantidade"
+            >
+              <ChevronRight className="h-5 w-5" />
+            </Button>
+          </div>
+        </div>
+
+        <Button
+          variant="ghost"
+          size="lg"
+          title="Remover produto"
+          aria-label="Remover produto"
+          className="group hover:cursor-pointer"
+        >
+          <Trash2 className="text-gray-600 transition-all duration-150 ease-linear group-hover:text-rose-500" />
+        </Button>
+      </div>
+    </div>
+  )
+}
--- src/components/cart-sheet/index.tsx
+++ src/components/cart-sheet/index.tsx
@@ -1,5 +1,8 @@
+import { formatCurrency } from '@/functions/format-currency'
 import { useCart } from '@/hooks/cart/use-cart'
 
+import { Button } from '../ui/button'
+import { Separator } from '../ui/separator'
 import {
   Sheet,
   SheetContent,
@@ -7,13 +10,21 @@ import {
   SheetHeader,
   SheetTitle,
 } from '../ui/sheet'
+import { CartItems } from './components/cart-items'
 
 export function CartSheet() {
   const { isOpen, toggleCart, products } = useCart()
+  const totalGrossPrice = products.reduce((acc, product) => {
+    return acc + product.oldPrice * product.quantity
+  }, 0)
+  const totalDiscount = products.reduce((acc, product) => {
+    return acc + (product.oldPrice - product.currentPrice) * product.quantity
+  }, 0)
+  const totalPrice = totalGrossPrice - totalDiscount
 
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-      <SheetContent>
+      <SheetContent className="w-[90%]">
         <SheetHeader>
           <SheetTitle className="text-goodycosmetics-primary-500 text-center font-normal uppercase">
             Sua cesta
@@ -23,23 +34,41 @@ export function CartSheet() {
           </SheetDescription>
         </SheetHeader>
         {products.length > 0 ? (
-          <div className="flex w-full flex-col gap-2">
-            {products.map((product) => (
-              <div
-                key={product.id}
-                className="flex w-full items-center justify-between rounded border border-gray-200 p-2"
-              >
-                <span className="text-sm font-light text-gray-500">
-                  {product.name}
-                </span>
-                <span className="text-sm font-light text-gray-500">
-                  {product.currentPrice}
-                </span>
-                <span className="text-sm font-light text-gray-500">
-                  {product.quantity}
-                </span>
+          <div className="flex h-full w-full flex-col px-4 pb-4">
+            <div className="flex w-full flex-1 flex-col gap-5">
+              {products.map((product) => (
+                <CartItems key={product.id} product={product} />
+              ))}
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <div className="flex w-full flex-col gap-2 rounded-[10px] border border-gray-200 p-4">
+                <div className="flex items-center justify-between text-sm font-light uppercase">
+                  <span>Subtotal</span>
+                  <span>{formatCurrency(totalGrossPrice)}</span>
+                </div>
+                <Separator className="bg-gray-100" />
+                <div className="flex items-center justify-between text-sm font-light uppercase">
+                  <span>Descontos</span>
+                  <span>
+                    {'-'} {formatCurrency(totalDiscount)}
+                  </span>
+                </div>
+                <Separator className="bg-gray-100" />
+                <div className="flex items-center justify-between text-sm font-normal uppercase">
+                  <span>Total</span>
+                  <span>{formatCurrency(totalPrice)}</span>
+                </div>
               </div>
-            ))}
+
+              <Button
+                type="button"
+                aria-label="Finalizar pedido"
+                className="bg-goodycosmetics-primary-500 hover:bg-goodycosmetics-primary-600 h-12 w-full font-light text-white uppercase transition-all duration-150 ease-linear hover:cursor-pointer"
+              >
+                Finalizar pedido
+              </Button>
+            </div>
           </div>
         ) : (
           <div className="flex w-full justify-center">
