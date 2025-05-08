3e46edb927f9f758a83e9f309ab31b24adf15476
Matheus <matheusfariasdasneves@gmail.com>
2025-05-08 16:12:17 -0300

    feat: add social media links

--- src/components/social-media/index.tsx
+++ src/components/social-media/index.tsx
@@ -4,11 +4,18 @@ import Link from 'next/link'
 import instagram from '@/assets/instagram.svg'
 import tikTok from '@/assets/tik-tok.svg'
 import whatsapp from '@/assets/whatsapp.svg'
+import { env } from '@/configs/env-configs'
 
 export function SocialMedia() {
+  const whatsappNumber = env.NEXT_PUBLIC_WHATSAPP_NUMBER
+
   return (
     <div className="flex items-center gap-3">
-      <Link href={'*'}>
+      <Link
+        href={'https://www.instagram.com/goodycosmeticos?igsh=ZjB3OGxsOHhmNW43'}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Image
           src={instagram}
           alt="Instagram"
@@ -17,7 +24,11 @@ export function SocialMedia() {
           className="h-auto w-auto"
         />
       </Link>
-      <Link href={'*'}>
+      <Link
+        href={'https://www.tiktok.com/@goodycosmeticos?_t=ZM-8wBb7eYutpm&_r=1'}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Image
           src={tikTok}
           alt="Tik Tok"
@@ -26,7 +37,11 @@ export function SocialMedia() {
           className="h-auto w-auto"
         />
       </Link>
-      <Link href={'*'}>
+      <Link
+        href={`https://wa.me/${whatsappNumber}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Image
           src={whatsapp}
           alt="Whatsapp"
